ef65a215c62162036ff62ff03dff5595da8ac574
Damarla Tarun Sai <tarunsai31302@gmail.com>
2023-10-20 20:51:53 -0400

    Update dns_forwarder.py
    Updated on 10/20/23 8:51 PM - Tarun Damarla

--- dns_forwarder.py
+++ dns_forwarder.py
@@ -1,16 +1,14 @@
-from dnslib import DNSRecord,QTYPE
+from dnslib import DNSRecord, QTYPE
+import argparse
 import socket
 import requests
 import base64
-from scapy.all import DNS, DNSQR, IP, UDP, Ether, sendp,DNSRR
 import dns.message
 import dns.rcode
 
-
-
-def initialize(host,port):
+def initialize(host, port):
     # Define the host and port to listen on
-    
+
     # Create a socket to listen for incoming DNS requests
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     server_socket.bind((host, port))
@@ -22,13 +20,13 @@ def messageFromDig(server_socket):
     data, ipaddress_port = server_socket.recvfrom(1024)
     # print(data)
     dns_request = DNSRecord.parse(data)
-    
+
     print(f"Received DNS request from {ipaddress_port}\n")
-    print("dig message request from out client+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++start\n")
-    print(dns_request,"\n\n")
+    print("dig message request from our client +++++++++++++++++++++++++++++++ start\n")
+    print(dns_request, "\n\n")
     # print(dns_request.questions)
-    print("dig message request from our client+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++end\n")
-    return data,ipaddress_port,dns_request.questions,dns_request.questions[0].qname,dns_request.questions[0].qtype
+    print("dig message request from our client +++++++++++++++++++++++++++++++ end\n")
+    return data, ipaddress_port, dns_request.questions, dns_request.questions[0].qname, dns_request.questions[0].qtype
 
 def binary_to_base64url(binary_data):
     # Encode the binary data to base64
@@ -40,8 +38,7 @@ def binary_to_base64url(binary_data):
     # Remove any padding '=' characters
     return base64url_encoded.rstrip('=')
 
-
-def connectToServer(ipaddress,port,path,dnsmessage):
+def connectToServer(ipaddress, port, path, dnsmessage):
     # Define the IP address and port (if needed).
     ip_address = ipaddress  # Replace with the target IP address
     port = port  # Replace with the target port for HTTPS (usually 443)
@@ -51,9 +48,9 @@ def connectToServer(ipaddress,port,path,dnsmessage):
 
     # Define the parameters you want to include in the request as a dictionary.
     params = {
-        "method" :"GET",
-        "accept" : "application/dns-message",
-        "dns":binary_to_base64url(dnsmessage)
+        "method": "GET",
+        "accept": "application/dns-message",
+        "dns": binary_to_base64url(dnsmessage)
     }
 
     # Construct the full URL using the IP address, port, and path.
@@ -69,15 +66,14 @@ def connectToServer(ipaddress,port,path,dnsmessage):
     else:
         print(f"Request failed with status code {response.status_code}")
         return response
-def communicateMessageBackToDig(server_socket,data,client_address):
+
+def communicateMessageBackToDig(server_socket, data, client_address):
     server_socket.sendto(data, client_address)
 
-
-def presentInDenyList(dnsmessage,denylist):
-    print("deny list is ====",denylist)
+def presentInDenyList(dnsmessage, denylist):
+    print("deny list is ====", denylist)
     for x in denylist:
-        # print(x)
-        if x==dnsmessage:
+        if x == dnsmessage:
             return True
     return False
 
@@ -103,45 +99,63 @@ def convert_to_nxdomain(response_data):
     except dns.exception.DNSException as e:
         print(f"Error converting to NXDOMAIN: {e}")
         return None
-   # !!!!!!!!!!!!!!!!!!!!!! code assumes there is only one question per dig
-def main():
-    print("hello world===============================================================")
-    doh_server_address="8.8.8.8"
-    doh_port=443
-    denylist_filename="deny_list.txt"
-    querylog_filename="queries.log"
-    server_socket=initialize('0.0.0.0',12345)
-    denylist=[]
+
+def main(args):
+    print("hello world ===============================================================")
+    if args.doh:
+        # If --doh is used, set the default DOH server to "1.1.1.1"
+        doh_server_address = "1.1.1.1"
+    else:
+        doh_server_address = str(args.DOH_SERVER)
+    
+    doh_port = 443
+    denylist_filename = args.DENY_LIST_FILE
+    querylog_filename = args.LOG_FILE
+    
+    # Check if both --doh and --doh_server are used simultaneously
+    if args.doh and args.DOH_SERVER:
+        print("Error: Cannot use --doh and --doh_server together.")
+        return
+    
+    server_socket = initialize('0.0.0.0', 53)
+    denylist = []
+    
     with open(denylist_filename, 'r') as file:
-    # Read each line from the file and split it into a list of strings
+        # Read each line from the file and split it into a list of strings
         for line in file:
             denylist.append(line.strip())
+    
     while True:
-        dns_request,ipaddress_port,dns_request_message,dns_request_question,dns_request_question_type=messageFromDig(server_socket)
-        requestedDnsRequestInDenyList=presentInDenyList(dns_request_question,denylist)
+        dns_request, ipaddress_port, dns_request_message, dns_request_question, dns_request_question_type = messageFromDig(server_socket)
+        requestedDnsRequestInDenyList = presentInDenyList(dns_request_question, denylist)
+        
         if requestedDnsRequestInDenyList:
             print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n")
-            print(dns_request_question," in denylist\n")
+            print(dns_request_question, " in denylist\n")
             print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n")
-            nxdomain_response_data=convert_to_nxdomain(dns_request)
-            communicateMessageBackToDig(server_socket,nxdomain_response_data,ipaddress_port)#since UDP protocol cannot say if it was sent
+            nxdomain_response_data = convert_to_nxdomain(dns_request)
+            communicateMessageBackToDig(server_socket, nxdomain_response_data, ipaddress_port)  # since UDP protocol cannot say if it was sent
             with open(querylog_filename, 'a+') as file:
                 file.write(f"{dns_request_question} {QTYPE[dns_request_question_type]} DENY\n")
         else:
             print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n")
-            print(dns_request_question,"not in denylist\n")
+            print(dns_request_question, " not in denylist\n")
             print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n")
-            response=connectToServer(doh_server_address,doh_port,"/dns-query",dns_request)
-            print("Response Content from doh server:===============================================================start")
-            #print(response,"\n\n")
-            #print(response.content)
-            #print("\n\n")
-            print(DNSRecord.parse(response.content))
-            print("Response Content from doh server end:===============================================================end")
-            communicateMessageBackToDig(server_socket,response.content,ipaddress_port)#since UDP protocol cannot say if it was sent
+            response = connectToServer(doh_server_address, doh_port, "/dns-query", dns_request)
+            print("Response Content from doh server: ================================================= start")
+            #print(DNSRecord.parse(response.content))
+            print("Response Content from doh server: ================================================= end")
+            communicateMessageBackToDig(server_socket, response.content, ipaddress_port)  # since UDP protocol cannot say if it was sent
             with open(querylog_filename, 'a+') as file:
                 file.write(f"{dns_request_question} {QTYPE[dns_request_question_type]} ALLOW\n")
-        
-    
+
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", dest="DST_IP", help="Destination DNS server IP")
+    parser.add_argument("-f", dest="DENY_LIST_FILE", help="File containing domains to block")
+    parser.add_argument("-l", dest="LOG_FILE", help="Log file for query results")
+    parser.add_argument("--doh", action="store_true", help="Use default upstream DoH server")
+    parser.add_argument("--doh_server", dest="DOH_SERVER", help="Use this upstream DoH server")
+
+    args = parser.parse_args()
+    main(args)

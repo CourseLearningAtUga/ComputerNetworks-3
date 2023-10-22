8d45d96cffc1f1d1fbd534201e42c17c23ace2ca
Damarla Tarun Sai <tarunsai31302@gmail.com>
2023-10-22 11:18:25 -0400

    Update dns_forwarder.py

--- dns_forwarder.py
+++ dns_forwarder.py
@@ -8,38 +8,26 @@ import dns.message
 import dns.rcode
 
 def initialize(host, port):
-    # Define the host and port to listen on
-
-    # Create a socket to listen for incoming DNS requests
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     server_socket.bind((host, port))
-
     print(f"Listening for DNS requests on {host}:{port}")
     return server_socket
 
 def messageFromDig(server_socket):
     data, ipaddress_port = server_socket.recvfrom(1024)
-    # print(data)
     dns_request = DNSRecord.parse(data)
-
     print(f"Received DNS request from {ipaddress_port}\n")
     print("dig message request from our client +++++++++++++++++++++++++++++++ start\n")
     print(dns_request, "\n\n")
-    # print(dns_request.questions)
     print("dig message request from our client +++++++++++++++++++++++++++++++ end\n")
     return data, ipaddress_port, dns_request.questions, dns_request.questions[0].qname, dns_request.questions[0].qtype
 
 def binary_to_base64url(binary_data):
-    # Encode the binary data to base64
     base64_encoded = base64.b64encode(binary_data).decode('utf-8')
-
-    # Replace '+' with '-' and '/' with '_'
     base64url_encoded = base64_encoded.replace('+', '-').replace('/', '_')
 
-    # Remove any padding '=' characters
     return base64url_encoded.rstrip('=')
 def connectToDnsServer(ip_address,port, dnsmessage):
-    # Create a UDP socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     try:
@@ -52,26 +40,16 @@ def connectToDnsServer(ip_address,port, dnsmessage):
     finally:
         sock.close()
 
-
-        
 def connectToDohServer(ip_address, port, path, dnsmessage):
-
-    # Define the parameters you want to include in the request as a dictionary.
     params = {
         "method": "GET",
         "accept": "application/dns-message",
         "dns": binary_to_base64url(dnsmessage)
     }
-
-    # Construct the full URL using the IP address, port, and path.
     url = f"https://{ip_address}:{port}{path}"
-
-    # Send the GET request with the specified parameters.
-    response = requests.get(url, params=params)  # Set 'verify' to False to ignore SSL certificate validation (for testing only)
-
-    # Check if the request was successful (status code 200).
+    response = requests.get(url, params=params) 
+    
     if response.status_code == 200:
-        # Print the response content (the content returned by the server).
         return response
     else:
         print(f"Request failed with status code {response.status_code}")
@@ -89,21 +67,12 @@ def presentInDenyList(dnsmessage, denylist):
 
 def convert_to_nxdomain(response_data):
     try:
-        # Parse the DNS response data
         response = dns.message.from_wire(response_data)
-
-        # Create an NXDOMAIN response
         nxdomain_response = dns.message.make_response(response)
-
-        # Set the response code to NXDOMAIN (RCODE 3)
         nxdomain_response.set_rcode(dns.rcode.NXDOMAIN)
-
-        # Remove any answer records, authority records, and additional records
         nxdomain_response.answer = []
         nxdomain_response.authority = []
         nxdomain_response.additional = []
-
-        # Return the NXDOMAIN response in binary format
         return nxdomain_response.to_wire()
 
     except dns.exception.DNSException as e:
@@ -116,35 +85,29 @@ def main(args):
         return
 
     if args.doh:
-        # If --doh is used, set the default DOH server to "1.1.1.1"
         doh_server_address = "1.1.1.1"
     else:
         doh_server_address = str(args.DOH_SERVER)
-    # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ important variables +++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        
     doh_port = 443
     denylist_filename = args.DENY_LIST_FILE
     querylog_filename = args.LOG_FILE
     dns_server=args.DST_IP
     dns_port=53
-    # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ important variables +++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    
-    # Check if both --doh and --doh_server are used simultaneously
+
     if args.doh and args.DOH_SERVER:
         print("Error: Cannot use --doh and --doh_server together.")
         return
-    
+        
     server_socket = initialize('0.0.0.0', 53)
     denylist = []
     
     with open(denylist_filename, 'r') as file:
-        # Read each line from the file and split it into a list of strings
         for line in file:
             denylist.append(line.strip())
-    
     while True:
         dns_request, ipaddress_port, dns_request_message, dns_request_question, dns_request_question_type = messageFromDig(server_socket)
         requestedDnsRequestInDenyList = presentInDenyList(dns_request_question, denylist)
-        
         if requestedDnsRequestInDenyList:
                 print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n")
                 print(dns_request_question, " in denylist\n")
@@ -184,6 +147,5 @@ if __name__ == "__main__":
     parser.add_argument("-l", dest="LOG_FILE", help="Log file for query results")
     parser.add_argument("--doh", action="store_true", help="Use default upstream DoH server")
     parser.add_argument("--doh_server", dest="DOH_SERVER", help="Use this upstream DoH server")
-
     args = parser.parse_args()
     main(args)
